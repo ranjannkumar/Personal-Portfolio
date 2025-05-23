a10e7999c8de632fe0ef7acb8eca994200ac5ecd
Ranjan kumar <ranjankumar5673@gmail.com>
2025-05-23 23:43:19 +0530

    starbackground added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
 export const Home = () => {
@@ -8,6 +9,7 @@ export const Home = () => {
       <ThemeToggle />
 
       {/* Background Effects */}
+      <StarBackground />
 
       {/* Navbar */}
 
