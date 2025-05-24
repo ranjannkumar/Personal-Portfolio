b02b44a2c39d20b2c122c48a29663a197c5b9e42
Ranjan kumar <ranjankumar5673@gmail.com>
2025-05-24 19:56:39 +0530

    footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { AboutSection } from "../components/AboutSection";
 import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { ProjectsSection } from "../components/ProjectSection";
@@ -30,6 +31,7 @@ export const Home = () => {
       </main>
 
       {/* Footer */}
+      <Footer/>
     </div>
   );
 };
